56a2e3f28dad2965de4534ad8a3cb65e827ceadb
lanceredfearn <lance.c.redfearn.mil@swf.army.mil>
2022-09-22 08:13:09 -0500

    initial changes

--- hw1-template/q4/Client.java
+++ hw1-template/q4/Client.java
@@ -3,87 +3,97 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Client {
-  public static void main (String[] args) throws IOException {
-    String hostAddress;
-    int tcpPort;
-    int udpPort;
+    public static void main(String[] args) throws IOException {
+        String hostAddress;
+        int tcpPort;
+        int udpPort;
+        boolean tcpMode = true;
 
-    if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <hostAddress>: the address of the server");
-      System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(3) <udpPort>: the port number for UDP connection");
-      System.exit(-1);
+        if (args.length != 3) {
+            System.out.println("ERROR: Provide 3 arguments");
+            System.out.println("\t(1) <hostAddress>: the address of the server");
+            System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
+            System.out.println("\t(3) <udpPort>: the port number for UDP connection");
+            System.exit(-1);
+        }
+
+        hostAddress = args[0];
+        tcpPort = Integer.parseInt(args[1]);
+        udpPort = Integer.parseInt(args[2]);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split(" ");
+
+            if (tokens[0].equals("setmode")) {
+                // TODO: set the mode of communication for sending commands to the server
+                // and display the name of the protocol that will be used in future
+                if (tokens[1].equals("t") || tokens[1].equals("T")) {
+                    System.out.println("The communication mode is set to TCP.");
+                } else if (tokens[1].equals("u") || tokens[1].equals("U")) {
+                    tcpMode = false;
+                    System.out.println("The communication mode is set to UDP.");
+                }
+
+            } else if (tokens[0].equals("purchase")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+            } else if (tokens[0].equals("cancel")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+            } else if (tokens[0].equals("search")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+            } else if (tokens[0].equals("list")) {
+                // TODO: send appropriate command to the server and display the
+                // appropriate responses form the server
+                System.out.println("You typed list!");
+                //String response = sendUDPRequest("list", hostAddress, udpPort);
+                String response = sendTCPRequest("list", hostAddress, tcpPort);
+                System.out.println(response);
+            } else {
+                System.out.println("ERROR: No such command");
+            }
+        }
     }
 
-    hostAddress = args[0];
-    tcpPort = Integer.parseInt(args[1]);
-    udpPort = Integer.parseInt(args[2]);
+    public static String sendUDPRequest(String requestParameters, String hostAddress, int udpPort) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress address = InetAddress.getByName(hostAddress);
+        byte[] buf = requestParameters.getBytes();
 
-    Scanner sc = new Scanner(System.in);
-    while(sc.hasNextLine()) {
-      String cmd = sc.nextLine();
-      String[] tokens = cmd.split(" ");
-
-      if (tokens[0].equals("setmode")) {
-        // TODO: set the mode of communication for sending commands to the server 
-        // and display the name of the protocol that will be used in future
-      }
-      else if (tokens[0].equals("purchase")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("cancel")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("list")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-        System.out.println("You typed list!");
-        //String response = sendUDPRequest("list", hostAddress, udpPort);
-        String response = sendTCPRequest("list", hostAddress, tcpPort);
-        System.out.println(response);
-      } else {
-        System.out.println("ERROR: No such command");
-      }
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, udpPort);
+        socket.send(packet);
+        buf = new byte[1024];
+        DatagramPacket received = new DatagramPacket(buf, 1024);
+        socket.receive(received);
+        String serverResponse = new String(received.getData(), 0, received.getLength());
+        socket.close();
+        return serverResponse;
     }
-  }
 
-  public static String sendUDPRequest(String requestParameters, String hostAddress, int udpPort) throws IOException {
-    DatagramSocket socket = new DatagramSocket();
-    InetAddress address = InetAddress.getByName(hostAddress);
-    byte[] buf = requestParameters.getBytes();
+    public static String sendTCPRequest(String requestParameters, String hostAddress, int tcpPort) throws IOException {
+        Socket serverSocket = new Socket(InetAddress.getByName(hostAddress), tcpPort);
 
-    DatagramPacket packet = new DatagramPacket(buf, buf.length, address, udpPort);
-    socket.send(packet);
-    buf = new byte[1024];
-    DatagramPacket received = new DatagramPacket(buf, 1024);
-    socket.receive(received);
-    String serverResponse = new String(received.getData(), 0, received.getLength());
-    socket.close();
-    return serverResponse;
-  }
+        System.out.println("Connected to Server: " + serverSocket.toString());
 
-  public static String sendTCPRequest(String requestParameters, String hostAddress, int tcpPort) throws IOException {
-    Socket serverSocket = new Socket(InetAddress.getByName(hostAddress), tcpPort);
+        PrintWriter toServer = new PrintWriter(serverSocket.getOutputStream(), true);
+        BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
 
-    System.out.println("Connected to Server: " + serverSocket.toString());
+        toServer.println(requestParameters);
+        String serverResponse = fromServer.readLine();
 
-    PrintWriter toServer = new PrintWriter(serverSocket.getOutputStream(), true);
-    BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-
-    toServer.println(requestParameters);
-    String serverResponse = fromServer.readLine();
-
-    serverSocket.close();
-    toServer.close();
-    fromServer.close();
-    return serverResponse;
-  }
+        serverSocket.close();
+        toServer.close();
+        fromServer.close();
+        return serverResponse;
+    }
 }
 
